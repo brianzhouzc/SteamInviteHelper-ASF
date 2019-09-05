2c8ae7e11ec457cb55d3279800962d567db7082d
Brian <me@brianzhou.co>
2019-09-05 16:41:33 -0700

    Removed Steam64ToSteam32() function and use Steamkit2 instead

--- SteamInviteHelper-ASF/WebRequestsHelper.cs
+++ SteamInviteHelper-ASF/WebRequestsHelper.cs
@@ -1,4 +1,5 @@
 ﻿using ArchiSteamFarm;
+using SteamKit2;
 using System;
 using System.Globalization;
 using System.Net.Http;
@@ -13,25 +14,10 @@ namespace SteamInviteHelper_ASF
 
         public static async Task<bool> StreamRepIsScammer(ulong steam64ID)
         {
-            string url = "http://steamrep.com/id2rep.php?steamID32=" + Steam64ToSteam32(steam64ID);
+            string url = "http://steamrep.com/id2rep.php?steamID32=" + new SteamID(steam64ID).Render();
             string result = await client.GetStringAsync(url);
 
             return result.Contains("SCAMMER");
-        }
-
-        private static string Steam64ToSteam32(ulong communityId)
-        {
-            if (communityId < 76561197960265729L || !Regex.IsMatch(communityId.ToString((IFormatProvider)CultureInfo.InvariantCulture), "^7656119([0-9]{10})$"))
-                return string.Empty;
-            communityId -= 76561197960265728L;
-            ulong num = communityId % 2L;
-            communityId -= num;
-            string input = string.Format("STEAM_0:{0}:{1}", num, (communityId / 2L));
-            if (!Regex.IsMatch(input, "^STEAM_0:[0-1]:([0-9]{1,10})$"))
-            {
-                return string.Empty;
-            }
-            return input;
         }
     }
 }
